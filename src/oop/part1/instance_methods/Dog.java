f3400f962def5c2d993bade11986d973645bc0c0
Alexinoo <mwangialex26@gmail.com>
2024-02-03 15:07:06 +0300

    Instance methods in Java

---
+++ src/oop/part1/instance_methods/Dog.java
@@ -0,0 +1,8 @@
+package oop.part1.instance_methods;
+
+public class Dog {
+
+    public void bark(){
+        System.out.println("Woof!!");
+    }
+}
---
+++ src/oop/part1/instance_methods/Main.java
@@ -0,0 +1,8 @@
+package oop.part1.instance_methods;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog rex = new Dog();
+        rex.bark();
+    }
+}
