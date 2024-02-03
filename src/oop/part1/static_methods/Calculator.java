f98310ed92d463a8c4eed1ed92dc1248f4b95496
Alexinoo <mwangialex26@gmail.com>
2024-02-03 14:58:02 +0300

    Static methods in Java

---
+++ src/oop/part1/static_methods/Calculator.java
@@ -0,0 +1,8 @@
+package oop.part1.static_methods;
+
+public class Calculator {
+
+    public static void printSUm(int a,int b){
+        System.out.println("sum = "+(a+b));
+    }
+}
---
+++ src/oop/part1/static_methods/Main.java
@@ -0,0 +1,14 @@
+package oop.part1.static_methods;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator.printSUm(5,10);
+        printHello();
+    }
+
+    public static void printHello(){
+        System.out.println("Hello");
+    }
+}
