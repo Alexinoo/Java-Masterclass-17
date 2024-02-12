e83061506a68f6499d928c82a474980e6c5c03a7
Alexinoo <mwangialex26@gmail.com>
2024-02-12 17:20:13 +0300

    Conditional Statements

---
+++ src/Fundamentals/basics/ConditionalStatement.java
@@ -0,0 +1,11 @@
+package Fundamentals.basics;
+
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        boolean isAlien = false;
+
+        if(isAlien == false)
+            System.out.println("I am scared of Alien");
+
+    }
+}
