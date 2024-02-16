04119e01b3b75ddec3f9584a5c9bff3441478b33
Alexinoo <mwangialex26@gmail.com>
2024-02-16 14:06:20 +0300

    Inheritance In java - Part 1

---
+++ src/oop/part1/inheritance/inheritance_part_1/Dog.java
@@ -0,0 +1,8 @@
+package oop.part1.inheritance.inheritance_part_1;
+
+public class Dog extends Animal {
+    public Dog(){
+        // super(); -- calls parent's class default constructor
+        super("German Shepherd","Big",50);
+    }
+}
