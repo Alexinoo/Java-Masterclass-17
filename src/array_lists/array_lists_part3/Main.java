9f6469b6724cd5153738d4ac71091bef6948c2a2
Alex Mwangi <alex@stl-horizon.com>
2024-02-19 16:20:12 +0300

    ArrayList Part 3

--- src/array_lists/array_lists_part3/Main.java
+++ src/array_lists/array_lists_part3/Main.java
@@ -111,7 +111,7 @@ public class Main {
 
         var groceryArray = groceries.toArray(new String[groceries.size()]);
 
-        System.out.println(Arrays.toString(groceryArray);
+        System.out.println(Arrays.toString(groceryArray));
 
 
 
