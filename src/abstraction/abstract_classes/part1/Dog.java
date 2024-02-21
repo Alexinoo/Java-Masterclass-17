c1b50705fd3b59650a95bcc7536eb2936282377c
Alex Mwangi <alex@stl-horizon.com>
2024-02-21 12:00:36 +0300

    Abstract Classes Part 1

---
+++ src/abstraction/abstract_classes/part1/Dog.java
@@ -0,0 +1,17 @@
+package abstraction.abstract_classes.part1;
+
+public class Dog extends Animal{
+    public Dog(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public void move(String speed) {
+
+    }
+
+    @Override
+    public void makeNoise() {
+
+    }
+}
---
+++ src/abstraction/abstract_classes/part1/Main.java
@@ -0,0 +1,8 @@
+package abstraction.abstract_classes.part1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
