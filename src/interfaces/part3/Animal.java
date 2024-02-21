9a7967a33006ea827dd158912306476313713d2a
Alex Mwangi <alex@stl-horizon.com>
2024-02-21 22:36:28 +0300

    Interfaces Part 3

---
+++ src/interfaces/part3/Animal.java
@@ -0,0 +1,73 @@
+package interfaces.part3;
+
+enum FlightStages implements Trackable{
+    GROUNDED,LAUNCH,CRUISE,DATA_COLLECTION;
+
+    @Override
+    public void track() {
+        if (this != GROUNDED){
+            System.out.println("Monitoring "+this);
+        }
+    }
+}
+
+record DragonFly(String name,String type) implements FlightEnabled{
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+
+class Satellite implements OrbitEarth{
+
+    @Override
+    public void achieveOrbit() {
+
+    }
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+
+interface OrbitEarth extends FlightEnabled{
+    void achieveOrbit();
+}
+
+interface FlightEnabled{
+    double MILE_TO_KM = 1.60934;
+    double KM_TO_MILES = 0.621371;
+    //public final double KM_TO_MILES= 0.621371; -- same as above
+    //public static final double KM_TO_MILES= 0.621371; -- same as above
+    void takeOff();
+    void land();
+    void fly();
+}
+interface Trackable{
+    void track();
+}
+public abstract class Animal {
+
+    public abstract void move();
+}
---
+++ src/interfaces/part3/Truck.java
@@ -0,0 +1,8 @@
+package interfaces.part3;
+
+public class Truck implements Trackable {
+    @Override
+    public void track() {
+        System.out.println(getClass().getSimpleName()+ " 's coordinates were recorded");
+    }
+}
