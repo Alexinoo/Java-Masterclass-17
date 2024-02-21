b44c38e144de16a020aeffe4028f4c72d057233b
Alex Mwangi <alex@stl-horizon.com>
2024-02-21 18:26:56 +0300

    Interfaces Part 1

---
+++ src/interfaces/part1/Animal.java
@@ -0,0 +1,14 @@
+package interfaces.part1;
+
+interface FlightEnabled{
+    void takeOff();
+    void land();
+    void fly();
+}
+interface Trackable{
+    void track();
+}
+public abstract class Animal {
+
+    public abstract void move();
+}
