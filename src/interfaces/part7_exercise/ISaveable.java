ecb5668ef6279d87226ca4c1d212b7d039ba1773
Alex Mwangi <alex@stl-horizon.com>
2024-02-22 22:34:08 +0300

    Coding Exercise 48: Interface

---
+++ src/interfaces/part7_exercise/ISaveable.java
@@ -0,0 +1,9 @@
+package interfaces.part7_exercise;
+
+import java.util.List;
+
+public interface ISaveable {
+
+   List<String> write();
+   void read(List<String> list);
+}
---
+++ src/interfaces/part7_exercise/Main.java
@@ -0,0 +1,19 @@
+package interfaces.part7_exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<ISaveable> list = new ArrayList<>();
+        list.add(new Player("Alex",15,20));
+        list.add(new Player("Tim",10,15));
+        list.add(new Monster("Lion",30,30));
+
+        for (ISaveable l: list ) {
+            System.out.println(l);
+        }
+    }
+}
