492ecd51274d4c6c0b31144d58e27a2dd709eefa
Alex Mwangi <alex@stl-horizon.com>
2024-03-01 07:34:45 +0300

    Code Setup(Deck of Cards) for java.util.Collections methods

---
+++ src/collections/part2_code_setup/Main.java
@@ -0,0 +1,12 @@
+package collections.part2_code_setup;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
+    }
+}
