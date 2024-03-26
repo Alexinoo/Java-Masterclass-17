ec33786ef359751e658601cc86ecf117bb1d1bfd
Alex Mwangi <alex@stl-horizon.com>
2024-03-26 06:56:30 +0300

    Challenge - Reading text from a file

---
+++ src/files_input_output/part13_reading_file_challenge/Main.java
@@ -0,0 +1,30 @@
+package files_input_output.part13_reading_file_challenge;
+
+public class Main {
+
+    /*
+     * Challenge - Reading from a Text file
+     *
+     * Pick some text of your choice (from a doc, online article, some wiki page)
+     * Read the text doc with one of the ()s we have covered
+     * Pick whichever () you want, but your program shd do the following
+     *
+     *  - Tokenize the text into words, remove any punctuation
+     *  - Ignore words with 5 char(s) or less
+     *  - Count the occurrences of each word
+     *  - Display the top 10 most used words
+     *
+     * The point of this exercise is to see if you can pick out what the articles
+     *  might be about by simply getting the most used words
+     *
+     *
+     * Bonus
+     * After you use 1 (), try a 2nd ()
+     * If you used a () that used a stream, try some code without a streaming () or
+     *  vice versa
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
