1e2f3b80fc2a7f5a799178a4919503ac9317afc0
Alex Mwangi <alex@stl-horizon.com>
2024-05-18 07:14:22 +0300

    VBox Layout

---
+++ src/JavaFx/part4_layouts/VBoxLayout.java
@@ -0,0 +1,16 @@
+package JavaFx.part4_layouts;
+
+public class VBoxLayout {
+
+    /*
+     * VBox Layout
+     * ............
+     * Stacks controls vertically
+     * Unlike the FlowPane, the controls will get cut off as we make the window smaller with a VBox
+     *
+     * Implementations
+     *  - Added VBox layout with 10 buttons
+     *
+     * Resize the window upwards or downwards and compare this with a FlowPane layout
+     */
+}
