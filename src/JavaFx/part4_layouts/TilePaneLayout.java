404bab833381113628fb6396b8823bf2d3499645
Alex Mwangi <alex@stl-horizon.com>
2024-05-18 07:14:45 +0300

    TilePane Layout

---
+++ src/JavaFx/part4_layouts/TilePaneLayout.java
@@ -0,0 +1,14 @@
+package JavaFx.part4_layouts;
+
+public class TilePaneLayout {
+    /*
+     * TilePane Layout
+     * .................
+     * With TilePane, every cell has been allocated the same width
+     * There are gaps between the buttons because some buttons aren't wide enough to occupy the width of their cell
+     * Now because each tile has a width of the largest button, there are 3 rows when we first run the application
+     *  rather than 2 that we saw when using FlowPane
+     * The buttons are centered within each cell & when we resize the window, we'll see the buttons are wrapped and unwrapped
+     *  as they did with FlowPanes
+     */
+}
