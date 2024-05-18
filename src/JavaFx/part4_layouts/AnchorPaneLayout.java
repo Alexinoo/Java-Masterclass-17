3814a3536d1dd68b2328c0aca82238d347e6e002
Alex Mwangi <alex@stl-horizon.com>
2024-05-18 07:15:42 +0300

    AnchorPane Layout

---
+++ src/JavaFx/part4_layouts/AnchorPaneLayout.java
@@ -0,0 +1,16 @@
+package JavaFx.part4_layouts;
+
+public class AnchorPaneLayout {
+    /*
+     * AnchorPane Layout
+     * .................
+     * This is another popular top level layout
+     * Allows us to anchor children to its edges which can influence what happens when the pane is resized
+     * For example
+     *  - We might want to anchor a title to the top of the pane so that when the window is resized, a bunch of space isn't
+     *    added between the title and the windows total bar
+     *  - We could anchor a HBox containing buttons to the bottom of the pane
+     *
+     *
+     */
+}
