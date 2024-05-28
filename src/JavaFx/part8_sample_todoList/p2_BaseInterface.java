bad0e302f010a72a67c9e24e1a25e139148a1ad7
Alex Mwangi <alex@stl-horizon.com>
2024-05-28 06:28:10 +0300

    Base Interface

---
+++ src/JavaFx/part8_sample_todoList/p2_BaseInterface.java
@@ -0,0 +1,16 @@
+package JavaFx.part8_sample_todoList;
+
+public class p2_BaseInterface {
+    /* Base Interface
+     * ..............
+     * Populate our ListView with todoItems
+     *  - Give it a unique fx:id first
+     *  - create an instance variable of type ListView
+     *  - call getItems.setAll() on the ListView variable
+     *      - pass todoItems ArrayList (list of the 5 items we created)
+     *  - Set the listview selection mode to single selection
+     *
+     *
+     *
+     */
+}
