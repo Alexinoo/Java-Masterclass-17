80ae1c53921147e4af0f26a18eb454362b5e8317
Alex Mwangi <alex@stl-horizon.com>
2024-05-28 06:29:11 +0300

    Formatting Dates

---
+++ src/JavaFx/part8_sample_todoList/p3_formatting_dates.java
@@ -0,0 +1,19 @@
+package JavaFx.part8_sample_todoList;
+
+public class p3_formatting_dates {
+
+    /*
+     * Formatting Dates
+     * ................
+     * Anytime we're displaying the date, we have to call toString() because the date is of type LocalDate
+     * It would be nice if we can display the date in a slightly better format
+     * Let's look at formatting that date, so it's a little bit easier to read, because for now it's hard to know which
+     *  part of the date is the month or date
+     *
+     * DateTimeFormatter
+     * ................
+     * We'll call DateTimeFormatter and call ofPattern() on this class and store that in a "dtf" variable
+     *  - Pass the format that we want our date to be formatted
+     *  - call format() on the dtf variable and pass the deadline date
+     */
+}
