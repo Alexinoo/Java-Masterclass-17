d3119b42438d460dd3ab249fd6c9e76cc47df17b
Alex Mwangi <alex@stl-horizon.com>
2024-06-01 08:00:18 +0300

    Exit menu functionality

---
+++ src/JavaFx/part8_sample_todoList/p12_ExitMenu.java
@@ -0,0 +1,12 @@
+package JavaFx.part8_sample_todoList;
+
+public class p12_ExitMenu {
+    /*
+     * Exit Menu
+     * .........
+     * Add the exit program functionality
+     *  - Add an fx:id to the menu item
+     *  - Add onAction and call handleExit() handler
+     *      - Call exit() on Platform class
+     */
+}
