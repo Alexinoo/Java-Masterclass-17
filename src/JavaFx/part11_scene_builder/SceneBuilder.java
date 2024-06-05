6d1eaf50543609da56bde846017dc5267b67a532
Alex Mwangi <alex@stl-horizon.com>
2024-06-05 07:01:58 +0300

    SceneBuilder

---
+++ src/JavaFx/part11_scene_builder/SceneBuilder.java
@@ -0,0 +1,17 @@
+package JavaFx.part11_scene_builder;
+
+public class SceneBuilder {
+
+    /*
+     * Scene Builder
+     * .............
+     * Scene builder is a GUI tool
+     * We can use it to build our UI by dragging and dropping JavaFX controls and containers onto a content area
+     *  - SceneBuilder then generates the fxml code for us
+     *  - It's written using JavaFX, so it's an example of a complex JavaFX application
+     *
+     * Overview of SceneBuilder
+     * ........................
+     *
+     */
+}
